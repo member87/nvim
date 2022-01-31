818af78c4d460517718162ac773abc8b6c4be546
jack <43416057+member87@users.noreply.github.com>
2022-01-31 16:09:38 +0000

    changed look

--- lua/modules/nvim-tree.lua
+++ lua/modules/nvim-tree.lua
@@ -1,3 +1,46 @@
+local g = vim.g
+
+g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
+-- g.nvim_tree_git_hl = git_status
+g.nvim_tree_highlight_opened_files = 0
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
+g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
+
+g.nvim_tree_window_picker_exclude = {
+   filetype = { "notify", "packer", "qf" },
+   buftype = { "terminal" },
+}
+
+g.nvim_tree_show_icons = {
+   folders = 1,
+   files = 1,
+   git = 1,
+}
+
+g.nvim_tree_icons = {
+   default = "",
+   symlink = "",
+   git = {
+      deleted = "",
+      ignored = "◌",
+      renamed = "➜",
+      staged = "✓",
+      unmerged = "",
+      unstaged = "✗",
+      untracked = "*",
+   },
+   folder = {
+      default = "",
+      empty = "",
+      empty_open = "",
+      open = "",
+      symlink = "",
+      symlink_open = "",
+   },
+}
+
+
 require'nvim-tree'.setup {
 	disable_netrw       = true,
 	hijack_netrw        = true,
@@ -6,7 +49,7 @@ require'nvim-tree'.setup {
 	auto_close          = false,
 	open_on_tab         = false,
 	hijack_cursor       = false,
-	update_cwd          = false,
+	update_cwd          = true,
 	update_to_buf_dir   = {
 		enable = true,
 		auto_open = true,
