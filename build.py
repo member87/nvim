1ade621af9821e610cee77857d6c5cf38e6a0232
jack <43416057+member87@users.noreply.github.com>
2022-09-13 19:31:57 +0100

    updated build

--- build.py
+++ build.py
@@ -1,11 +1,13 @@
-import re
+import re, requests
 
 plugins = []
 
+def get_description(plugin):
+    r = requests.get(f"https://api.github.com/repos/{plugin}")
+    print(r.json()["description"])
+
 with open("lua/core/plugins.lua") as f:
     content = f.read()
-
-    
     regex = 'use\("(.*?)"'
     plugins += re.findall(regex, content)
     regex = 'use\({\s+"(.*?)"'
@@ -19,7 +21,7 @@ with open("README.md") as f:
     new_readme += "## Plugins\n"
 
 for plugin in plugins:
-    new_readme += f"- [{plugin}](http://github.com/{plugin})\n"
+    new_readme += f"- [{plugin}](http://github.com/{plugin}) - {get_description(plugin)}\n"
 
 with open("README.md", "w") as f:
     f.write(new_readme)
