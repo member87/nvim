2097801f0d92eb884b5bb6c4e1ab45ec315604a9
jack <43416057+member87@users.noreply.github.com>
2023-11-10 22:53:44 +0000

    updated to lazy.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,231 +1,177 @@
-local packer_status_ok, packer = pcall(require, "packer")
-if not packer_status_ok then
-  return
-end
+return {
 
-packer.startup({
-  function(use)
-    -- Plugin manger
-    use("wbthomason/packer.nvim")
+  -- Lua Functions
+  {
+    "nvim-lua/plenary.nvim"
+  },
 
-    -- Lua functions
-    use("nvim-lua/plenary.nvim")
-
-    -- mason
-    use({
-      "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup()
-      end
-    })
-    use {
-      "williamboman/mason-lspconfig.nvim"
+  -- LSP
+  {
+    "williamboman/mason-lspconfig.nvim"
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+    end
+  },
+  {
+    "neovim/nvim-lspconfig"
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      require("configs.cmp-nvim")
+    end,
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp"
+  },
+  {
+    "hrsh7th/cmp-buffer"
+  },
+  {
+    "hrsh7th/cmp-path"
+  },
+  {
+    "hrsh7th/cmp-cmdline"
+  },
+  {
+    "ray-x/lsp_signature.nvim"
+  },
+  {
+    "hrsh7th/vim-vsnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
     }
+  },
+  {
+    "hrsh7th/cmp-vsnip"
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  },
 
-    use {
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-      config = function()
-        require("mason-tool-installer").setup({
-          auto_update = true,
-          run_on_start = true
-        })
-      end
-    }
+  -- Formatting 
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter"
+    },
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("configs.nvim-autopairs")
+    end,
+  },
 
-    -- builtin lsp
-    use("neovim/nvim-lspconfig")
+  -- Context
+  {
+    "wellle/context.vim",
+  },
 
-    use({
-      "hrsh7th/nvim-cmp",
-      config = function()
-        require("configs.cmp-nvim")
-      end,
-    })
-
-    -- LSP completion source
-    use("hrsh7th/cmp-nvim-lsp")
-
-    -- Buffer completion source
-    use({
-      "hrsh7th/cmp-buffer",
-      after = "nvim-cmp",
-    })
-
-    -- Path completion source
-    use({
-      "hrsh7th/cmp-path",
-      after = "nvim-cmp",
-    })
-
-    -- Command line completion source
-    use({
-      "hrsh7th/cmp-cmdline",
-      after = "nvim-cmp",
-    })
-
-    -- LSP signature
-    use({
-      "ray-x/lsp_signature.nvim",
-      after = "nvim-cmp",
-    })
-
-    -- Snippet engine
-    use({
-      "hrsh7th/vim-vsnip",
-      requires = {
-        "rafamadriz/friendly-snippets",
-      },
-    })
-
-    -- Snippet completion source
-    use({
-      "hrsh7th/cmp-vsnip",
-      after = "nvim-cmp",
-    })
-
-    -- Syntax highlighting
-    use({
-      "nvim-treesitter/nvim-treesitter",
+  -- UI
+  { 
+    "kyazdani42/nvim-tree.lua",
+    config = function()
+      require("configs.tree")
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      vim.notify = require("notify")
+    end,
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("configs.colorizer")
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    config = function()
+      require("configs.telescope")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
       config = function()
         require("configs.treesitter")
       end,
-    })
-
-    use({
-      "windwp/nvim-ts-autotag",
-      requires = "nvim-treesitter/nvim-treesitter",
-      config = function()
-        require('nvim-ts-autotag').setup()
-      end
-    })
-
-
-    use({
-      "wellle/context.vim",
-    })
-
-    -- Auto pairs
-    use({
-      "windwp/nvim-autopairs",
-      config = function()
-        require("configs.nvim-autopairs")
-      end,
-      after = "nvim-cmp",
-    })
-
-    -- Notifications
-    use({
-      "rcarriga/nvim-notify",
-      config = function()
-        vim.notify = require("notify")
-      end,
-    })
-
-    -- Highlight colours
-    use({
-      "norcalli/nvim-colorizer.lua",
-      config = function()
-        require("configs.colorizer")
-      end,
-      after = "nvim-treesitter",
-    })
-
-    -- File finder
-    use({
-      "nvim-telescope/telescope.nvim",
-      cmd = "Telescope",
-      config = function()
-        require("configs.telescope")
-      end,
-    })
-
-    -- Bufferline
-    use({
-      "akinsho/bufferline.nvim",
-      requires = 'nvim-tree/nvim-web-devicons',
-      config = function()
-        require("bufferline").setup {}
-      end
-    })
-
-    -- Statusline
-    use({
-      "nvim-lualine/lualine.nvim",
-      config = function()
-        require("configs.lualine")
-      end,
-    })
-
-    -- File explorer
-    use({
-      "kyazdani42/nvim-tree.lua",
-      config = function()
-        require("configs.tree")
-      end,
-    })
-
-    -- Colour Scheme
-    use({
-      "catppuccin/nvim",
-      config = function()
-        vim.cmd.colorscheme "catppuccin"
-        require("catppuccin").setup({
-          flavour = "mocha",
-        })
-      end,
-    })
-
-    -- Icons
-    use("kyazdani42/nvim-web-devicons")
-
-    -- Laravel
-    use("jwalton512/vim-blade")
-
-    use({
-      "folke/which-key.nvim",
-      config = function()
-        require("configs.whichkey")
-      end,
-    })
-
-    use({
-      "lewis6991/gitsigns.nvim",
-      config = function()
-        require("gitsigns").setup({
-          current_line_blame = true,
-          current_line_blame_opts = {
-            virt_text = true,
-            virt_text_pos = "eol", -- "eol" | "overlay" | "right_align"
-            delay = 500,
-            ignore_whitespace = false,
-          },
-        })
-      end
-    })
-
-
-    use({
-      "zbirenbaum/copilot.lua",
-      config = function()
-        require("copilot").setup({
-          suggestion = { enabled = false },
-          panel = { enabled = false },
-        })
-      end
-
-    })
-
-    use({
-      "zbirenbaum/copilot-cmp",
-      after = { "copilot.lua" },
-      config = function()
-        require("copilot_cmp").setup()
-      end
-    })
-
-    use({
-      "onsails/lspkind.nvim",
-    })
-  end,
-})
+  },
+  {
+    "akinsho/bufferline.nvim",
+    requires = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("bufferline").setup {}
+    end
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("configs.lualine")
+    end,
+  },
+  {
+    "kyazdani42/nvim-tree.lua",
+    config = function()
+      require("configs.tree")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    config = function()
+      vim.cmd.colorscheme "catppuccin"
+      require("catppuccin").setup({
+        flavour = "mocha",
+      })
+    end,
+  },
+  {
+    "kyazdani42/nvim-web-devicons"
+  },
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("configs.whichkey")
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol", -- "eol" | "overlay" | "right_align"
+          delay = 500,
+          ignore_whitespace = false,
+        },
+      })
+    end
+  },
+  {
+    "onsails/lspkind.nvim",
+  }
+}
