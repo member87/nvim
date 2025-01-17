a5eb62642046e6a232fd8a8f822fdcf3310797ab
Jack <43416057+member87@users.noreply.github.com>
2025-01-17 23:49:24 +0000

    enable build

--- build.py
+++ build.py
@@ -19,15 +19,15 @@ for file in os.listdir("lua/plugins"):
 print(plugins)
 
 
-# new_readme = ""
-# with open("README.md") as f:
-#     content = f.read()
-#     new_readme = content.split("## Plugins", 1)[0]
-#     new_readme += "## Plugins\n"
-#
-# for plugin in plugins:
-#     print(plugin)
-#     new_readme += f"- [{plugin}](http://github.com/{plugin}) - {get_description(plugin)}\n"
-#
-# with open("README.md", "w") as f:
-#     f.write(new_readme)
+new_readme = ""
+with open("README.md") as f:
+    content = f.read()
+    new_readme = content.split("## Plugins", 1)[0]
+    new_readme += "## Plugins\n"
+
+for plugin in plugins:
+    print(plugin)
+    new_readme += f"- [{plugin}](http://github.com/{plugin}) - {get_description(plugin)}\n"
+
+with open("README.md", "w") as f:
+    f.write(new_readme)
