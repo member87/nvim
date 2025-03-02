db1b41aacfee8df6de38a2c9e6f54bc07eec794d
Jack <43416057+member87@users.noreply.github.com>
2025-03-02 13:19:51 +0000

    updates

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -10,13 +10,14 @@ nvim_lsp.lua_ls.setup({
     },
   },
 })
-nvim_lsp.nil_ls.setup({
-  autostart = true,
-  settings = {
-    ['nil'] = {
-      formatting = {
-        command = { "nixfmt" },
-      },
-    },
-  },
-})
+
+local lspconfig = require("lspconfig")
+require("mason-lspconfig").setup_handlers {
+  -- This is a default handler that will be called for each installed server (also for new servers that are installed during a session)
+  function(server_name)
+    lspconfig[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+    }
+  end,
+}
