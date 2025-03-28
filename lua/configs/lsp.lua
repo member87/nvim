88329ba1be8eaf448ce3949bcd33e930ae412bed
Jack <43416057+member87@users.noreply.github.com>
2025-03-28 11:37:17 +0000

    cva tailwindcss

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -8,5 +8,19 @@ require("mason-lspconfig").setup_handlers {
   function (server_name)
     require("lspconfig")[server_name].setup({ capabilities = capabilities })
   end,
+  ["tailwindcss"] = function()
+    require("lspconfig").tailwindcss.setup({
+      capabilities = capabilities,
+      settings = {
+        tailwindCSS = {
+          experimental = {
+            classRegex = {
+              {    "cva\\(([^)(]*(?:\\([^)(]*(?:\\([^)(]*(?:\\([^)(]*\\)[^)(]*)*\\)[^)(]*)*\\)[^)(]*)*)\\)",}
+            },
+          },
+        },
+      },
+    })
+  end,
 }
 
